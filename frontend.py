65d990b1e963dc6ba7c34614ac0138c9ebdadd4e
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-03-16 20:03:37 +0500

    almost completed the frontend

--- frontend.py
+++ frontend.py
@@ -100,6 +100,12 @@ def start_gui(book_manager):
 
     reading_progress_button = Button(root, text="Reading Progress", padx=10, pady=10, command=reading_progress)
     reading_progress_button.grid(row=6, column=0, padx=10, pady=10)
+    
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            root.destroy()
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
 
     
